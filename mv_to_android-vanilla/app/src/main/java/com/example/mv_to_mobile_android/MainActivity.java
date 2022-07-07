1bea7465f619929333b75209728f9db792535dbf
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-07-07 14:57:12 +0900

    Update MainActivity.java

--- mv_to_android-vanilla/app/src/main/java/com/example/mv_to_mobile_android/MainActivity.java
+++ mv_to_android-vanilla/app/src/main/java/com/example/mv_to_mobile_android/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.mv_to_mobile_android;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-import android.view.WindowManager;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
@@ -62,10 +60,5 @@ public class MainActivity extends AppCompatActivity {
         View decoView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_IMMERSIVE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
         decoView.setSystemUiVisibility(uiOptions);
-        // ignore display cutout
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        }
     }
 }
